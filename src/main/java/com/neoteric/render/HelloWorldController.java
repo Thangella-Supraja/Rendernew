10cb0b10e1369e2d4f1ec9f0daaaf3c2ef4162cd
Supraja-Thangella <165669824+Supraja-Thangella@users.noreply.github.com>
2025-07-17 16:12:32 +0530

    first commit

---
+++ src/main/java/com/neoteric/render/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.neoteric.render;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @GetMapping("/api/message")
+    public String getMessage() {
+        return "Achieved CI/CD with help of render";
+    }
+
+}
---
+++ src/main/java/com/neoteric/render/RenderApplication.java
@@ -0,0 +1,12 @@
+package com.neoteric.render;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RenderApplication.class,args);
+    }
+
+}
